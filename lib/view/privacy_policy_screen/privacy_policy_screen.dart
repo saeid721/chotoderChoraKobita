760cf163b911ed67baa1a1229e6577bc4b4ff576
saeid721 <saeid721km@gmail.com>
2025-03-22 15:30:40 +0600

    update

--- lib/view/privacy_policy_screen/privacy_policy_screen.dart
+++ lib/view/privacy_policy_screen/privacy_policy_screen.dart
@@ -203,7 +203,7 @@ This privacy policy is effective as of 2025-02-01"""
               ),
               const GlobalText(
                 str:
-                    "If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at info@KaltiEngineering.com.",
+                    "If you have any questions regarding privacy while using the Application, or have questions about the practices, please contact the Service Provider via email at info@stitbd.com.",
                 color: ColorRes.dark,
                 fontSize: 12,
                 fontWeight: FontWeight.w400,
